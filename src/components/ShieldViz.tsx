b025a61bf650ee5cb479d658ef44f6966796a14b
shrigo <71424680+shrigo@users.noreply.github.com>
2026-06-23 16:01:24 -0400

    chore(assets): update hero graphic to pphr_shield.gif

--- src/components/ShieldViz.tsx
+++ src/components/ShieldViz.tsx
@@ -239,7 +239,7 @@ export default function ShieldViz(){
 
   if(!mounted) return(
     <div style={{position:'relative',width:'100%',maxWidth:713,margin:'0 auto'}}>
-      <Image src="/pp_hr_v2.gif" alt="PosturePilot Command Center"
+      <Image src="/pphr_shield.gif" alt="PosturePilot Command Center"
         width={713} height={713} unoptimized
         style={{width:'100%',height:'auto',display:'block'}}
         priority/>
@@ -260,7 +260,7 @@ export default function ShieldViz(){
 
   return(
     <div style={{position:'relative',width:'100%',maxWidth:713,margin:'0 auto'}}>
-      <Image src="/pp_hr_v2.gif" alt="PosturePilot Command Center"
+      <Image src="/pphr_shield.gif" alt="PosturePilot Command Center"
         width={713} height={713} unoptimized
         style={{width:'100%',height:'auto',display:'block'}}
         priority/>
